d15dbd1e36e067dca3c17cfb9915741b1c82bd00
Jakub Turski <yacoob@gmail.com>
2014-04-21 22:37:40 +0100

    Log things to syslog.

--- plexup/plexup.go
+++ plexup/plexup.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"io"
+	"log"
+	"log/syslog"
 	"net/http"
 )
 
+const logging_tag = "plexup"
 const address = ":25010"
 
 func OHai(w http.ResponseWriter, req *http.Request) {
@@ -12,6 +15,11 @@ func OHai(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	logger, err := syslog.New(syslog.LOG_USER|syslog.LOG_NOTICE, logging_tag)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	logger.Notice("Starting at addres: " + address)
 	http.HandleFunc("/", OHai)
 	http.ListenAndServe(address, nil)
 }
