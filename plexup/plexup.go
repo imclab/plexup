08504b121f0542c79aa0756ccb4c0c80e7f4dc02
Jakub Turski <yacoob@gmail.com>
2014-04-27 00:05:43 +0100

    Use a structure to store shared data.

--- plexup/plexup.go
+++ plexup/plexup.go
@@ -11,32 +11,36 @@ import (
 const logging_tag = "plexup"
 const address = ":25010"
 
-var pms_cmd exec.Cmd
-var logger syslog.Writer
+type plexup struct {
+	cmd    exec.Cmd
+	logger *syslog.Writer
+}
 
-func PlexOn(w http.ResponseWriter, req *http.Request) {
-	logger.Notice("Turning Plex Media Server on.")
-	pms_cmd.Start()
+func (p *plexup) on(w http.ResponseWriter, req *http.Request) {
+	p.logger.Notice("Turning Plex Media Server on.")
+	p.cmd.Start()
 	// http://127.0.0.1:32400/library/sections/all/refresh
 	io.WriteString(w, "on handler\n")
 }
 
-func PlexOff(w http.ResponseWriter, req *http.Request) {
-	logger.Notice("Turning Plex Media Server off.")
+func (p *plexup) off(w http.ResponseWriter, req *http.Request) {
+	p.logger.Notice("Turning Plex Media Server off.")
 	io.WriteString(w, "off handler\n")
 }
 
 func main() {
+	var pms = new(plexup)
 	logger, err := syslog.New(syslog.LOG_USER|syslog.LOG_NOTICE, logging_tag)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	logger.Notice("Starting at addres: " + address)
-	pms_cmd = exec.Cmd{
+	pms.logger = logger
+	pms.logger.Notice("Starting at addres: " + address)
+	pms.cmd = exec.Cmd{
 		Path: "/Applications/Plex Media Server.app/Contents/MacOS/Plex Media Server",
 		Dir:  "/",
 	}
-	http.HandleFunc("/on", PlexOn)
-	http.HandleFunc("/off", PlexOff)
+	http.HandleFunc("/on", pms.on)
+	http.HandleFunc("/off", pms.off)
 	http.ListenAndServe(address, nil)
 }
