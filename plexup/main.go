a6160c878749bc37671c26f96444d1ed90414a88
Jakub Turski <yacoob@gmail.com>
2014-05-01 01:12:21 +0100

    Implement a single goroutine controller.

--- plexup/main.go
+++ plexup/main.go
@@ -15,36 +15,48 @@ import (
 const loggingTag = "plexup"
 const plexupPort = 25010
 
-// Idea from gobry: one channel-triggered control gouroutine
-
 type plexup struct {
-	logger   *syslog.Writer
-	finished chan struct{}
+	running chan bool
+}
+
+func pmsController(c chan bool, logger *syslog.Writer) {
+	finished := make(chan struct{}, 1)
+	finished <- struct{}{}
+	for {
+		logger.Notice("controller: czekam na desiredState")
+		desiredState := <-c
+		logger.Notice("controller: dostałem nowy desiredState z kanału")
+		switch desiredState {
+		case true:
+			select {
+			default:
+				logger.Notice("controller: I think pms is already running")
+			case <-finished:
+				logger.Notice("Turning Plex Media Server on.")
+				cmd := exec.Command("/usr/bin/caffeinate", "/Applications/Plex Media Server.app/Contents/MacOS/Plex Media Server")
+				go func() {
+					cmd.Run()
+					finished <- struct{}{}
+				}()
+				// TODO: hit http://127.0.0.1:32400/library/sections/all/refresh
+			}
+		case false:
+			logger.Notice("Turning Plex Media Server off.")
+			gpid, _ := syscall.Getpgid(os.Getpid())
+			// TODO: avoid calling pkill
+			exec.Command("pkill", "-g", strconv.Itoa(gpid), "Plex Media Server").Run()
+		}
+	}
 }
 
 func (pms *plexup) on(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "on handler\n")
-	select {
-	default:
-		io.WriteString(w, "I think pms is already running.\n")
-		return
-	case <-pms.finished:
-		pms.logger.Notice("Turning Plex Media Server on.")
-	}
-	cmd := exec.Command("/usr/bin/caffeinate", "/Applications/Plex Media Server.app/Contents/MacOS/Plex Media Server")
-	go func() {
-		cmd.Run()
-		pms.finished <- struct{}{}
-	}()
-	// TODO: hit http://127.0.0.1:32400/library/sections/all/refresh
+	pms.running <- true
 }
 
 func (pms *plexup) off(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "off handler\n")
-	pms.logger.Notice("Turning Plex Media Server off.")
-	gpid, _ := syscall.Getpgid(os.Getpid())
-	// TODO: avoid calling pkill
-	exec.Command("pkill", "-g", strconv.Itoa(gpid), "Plex Media Server").Run()
+	pms.running <- false
 }
 
 func deathRattler(c chan os.Signal, logger *syslog.Writer) {
@@ -58,23 +70,22 @@ func deathRattler(c chan os.Signal, logger *syslog.Writer) {
 func main() {
 	var address = ":" + strconv.Itoa(plexupPort)
 	var err error
-
-	// Initialize control structure.
-	var pms = new(plexup)
-	pms.finished = make(chan struct{}, 1)
-	pms.finished <- struct{}{}
-	if pms.logger, err = syslog.New(syslog.LOG_USER|syslog.LOG_NOTICE, loggingTag); err != nil {
+	logger, err := syslog.New(syslog.LOG_USER|syslog.LOG_NOTICE, loggingTag)
+	if err != nil {
 		log.Fatalln(err)
 	}
-
-	// Main screen turn on.
-	pms.logger.Notice("Starting at addres: " + address)
 
 	// Handle signals
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGTERM)
-	go deathRattler(c, pms.logger)
+	go deathRattler(c, logger)
 
+	// Initialize control goroutine.
+	pms := plexup{running: make(chan bool)}
+	go pmsController(pms.running, logger)
+
+	// Main screen turn on.
+	logger.Notice("Starting at addres: " + address)
 	exec.Command("dns-sd", "-R", "plexup", "_plexup._tcp.", ".", strconv.Itoa(plexupPort), "pdl=application/plexup").Start()
 	http.HandleFunc("/on", pms.on)
 	http.HandleFunc("/off", pms.off)
